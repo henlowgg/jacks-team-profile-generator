d72a341b05f7a640bb6869b3783dd945d2d5e6b1
Jack Ault <henlowgg@outlook.com>
2021-10-30 13:48:00 -0400

    manager expecting

--- __tests__/manager.test.js
+++ __tests__/manager.test.js
@@ -1,3 +1,9 @@
 const Manager = require("../lib/manager");
 
 test("creates an Manager obj", () => {
+
+    const manager = new Manager("Jack", 27, "henlowgg@outlook.com", 13);
+
+    expect(manager.offNum).toEqual(expect.any(Number));
+});
+
